b9a3f2487e7c3b8613d56618d04c1ce78628d643
parker davis <theparkerevan>
2025-04-21 14:47:43 -0400

    move around tk window stuff

--- src/py_src/window.py
+++ src/py_src/window.py
@@ -5,8 +5,8 @@ import sys
 
 import rust_interfacer
 
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+import tkinter.ttk as ttk
 import tkinter.filedialog
 
 class Window:
@@ -22,13 +22,13 @@ class Window:
         self.set_up_logging()
 
         ### Main Window
-        self.root = Tk()
+        self.root = tk.Tk()
         self.root.resizable(width=False, height=False)
         self.root.geometry("800x500")
         self.root.title("DGR TOOLS")
         self.logger.info("window created")
 
-        bg = PhotoImage(file = "assets/background.png") 
+        bg = tk.PhotoImage(file = "assets/background.png") 
         self.background_label = ttk.Label(self.root, image=bg)
         self.background_label.place(x = 0, y = 0)
 
@@ -36,17 +36,99 @@ class Window:
         #main_frame.place(x=0, y=0)
 
         #self.header_label = ttk.Label(self.root, text="Gello Gorld!", width=100).place(x = 350, y = 25)
-        self.compile_button = ttk.Button(self.root, text="Compile", command=lambda: rust_interfacer.compile_lin()).place(x = 25, y = 75)
-        self.decompile_button = ttk.Button(self.root, text="Decompile", command=lambda: rust_interfacer.decompile_lin()).place(x = 25, y = 150)
+        self.compile_button   = tk.Button(self.root, text="Compile",   width=18, height=3, command=lambda:  self.compile_lin()).place(x = 25, y = 75)
+        self.decompile_button = tk.Button(self.root, text="Decompile", width=18, height=3, command=lambda: self.decompile_lin()).place(x = 25, y = 150)
 
         self.log_frame = ttk.Frame(self.root, width=400, height=200)
         self.log_frame.place(x=200, y=200)
 
-        self.file_log = Label(self.log_frame, text="", width=50)
+        self.file_log = tk.Label(self.log_frame, text="", justify="left")
         self.file_log.place(x=0, y=0)
         self.logger.info("widgets created")
 
+        ###
+        self.log_frame_lines = []
+
+        ###
+
         self.root.mainloop()
+    
+
+    def compile_lin(self):
+        ## CHOOSE INPUT
+        self.logger.info("opening file dialog")
+        input_filenames = tkinter.filedialog.askopenfilenames(initialdir = self.last_compile_input,
+                                        title = "Select a text file!",
+                                        filetypes = (("Text files", "*.txt*"),
+                                                    ("Text files", "*.txt*")))
+
+        if input_filenames == "":
+            self.logger.warning("user did not select file")
+            self.update_log("No file selected!")
+            return
+
+        self.last_compile_input = input_filenames[0].rsplit("/", 1)[0]
+
+        ## CHOOSE OUTPUT
+        output_folder = tkinter.filedialog.askdirectory(initialdir = self.last_compile_output,
+                                        title = "Select an output folder!")
+        
+        if output_folder == "":
+            self.logger.warning("user did not select output folder")
+            self.update_log("No folder selected!")
+            return
+        
+        self.last_compile_output = output_folder.rsplit("/", 1)[0]
+
+        for input_filename in input_filenames:
+            try:
+                dgrlin.compile(input_filename, output_folder)
+                self.update_log(f"File compiled: {input_filename}")
+            except RuntimeError as err:
+                self.update_log(err.__str__().split("\n")[0])
+            except:
+                print("Unknown Error")
+        
+        
+
+    def decompile_lin(self):
+        ## CHOOSE INPUT
+        self.logger.info("opening file dialog")
+        input_filenames = tkinter.filedialog.askopenfilenames(initialdir = self.last_decompile_input,
+                                        title = "Select a binary!",
+                                        filetypes = (("Linary files", "*.lin*"),
+                                                    ("Binary files", "*.bin*")))   
+        
+        ## For some unkown reason, 'askopenfilenames' returns a tuple when any file[s] is selected.
+        ## But if no files are selected, it returns an empty string.
+        if input_filenames == "":
+                self.logger.warning("user did not select file")
+                self.update_log("No file selected!")
+                return
+
+        self.last_decompile_input = input_filenames[0].rsplit("/", 1)[0]
+
+        ## CHOOSE OUTPUT
+        output_folder = tkinter.filedialog.askdirectory(initialdir = self.last_decompile_output,
+                                        title = "Select an output folder!")
+        
+        self.last_decompile_output = output_folder.rsplit("/", 1)[0]
+        
+        if output_folder == "":
+            self.logger.warning("user did not select output folder")
+            self.update_log("No folder selected!")
+            return
+
+        for input_filename in input_filenames:
+            try:
+                dgrlin.decompile(input_filename, output_folder)
+                self.update_log(f"File decompiled: {input_filename}")
+            except RuntimeError as err:
+                self.update_log(err.__str__().split("\n")[0])
+                return
+            except:
+                print("Unknown Error")
+
 
     def set_up_logging(self, level: int = logging.DEBUG) -> None:
         self.logger = logging.getLogger(__name__)
@@ -60,5 +142,10 @@ class Window:
 
         self.logger.addHandler(handler)
 
-    def update_log(new_line):
-        ...
+    def update_log(self, new_line):
+        if self.log_frame_lines.__len__() > 11:
+            self.log_frame_lines.pop(11)
+        
+        self.log_frame_lines.insert(0, new_line)
+
+        self.file_log.configure(text="\n".join(self.log_frame_lines))
